bf8de7359864d11c7e6d35584470b6fc70db456a
Dmitriy Lukichev <dmlukichev@gmail.com>
2017-10-03 20:21:50 +0300

    snake is done

--- snake/snake.js
+++ snake/snake.js
@@ -3,28 +3,95 @@ const lodash = _
 document.addEventListener('DOMContentLoaded', main)
 
 function main() {
-    var $canvas, $headSnake
+    var $canvas, $counter, $lostMessage
+    var snake
     var direction
-    var x, y
     var cx, cy
-    var flag
+    var hasLost
+    var doGrow
     var apples
+    var counter
+    var i
 
     apples = []
     $canvas = document.getElementById('canvas')
-    $headSnake = document.getElementById('snake-head')
-    x = 100
-    y = 100
-    cx = 400
-    cy = 400
-    flag = false
+    $counter = document.getElementById('counter')
+    $lostMessage = document.getElementById('lost-message')
+
+    $lostMessage.style.display ='none'
+    initSnake(3)
+    placeApple()
+
+
+    cx = 30
+    cy = 30
+    counter = 0
+    hasLost = false
+    doGrow = false
     document.addEventListener('keydown', handleKeyPress)
 
     setInterval(move, 100)
 
+    function initSnake(length) {
+        snake = []
+
+        var i
+        var t = {}
+
+        t.$el = document.createElement('div')
+        t.$el.id = 'snake-head'
+        $canvas.appendChild(t.$el)
+        t.x = 15
+        t.y = 15
+
+
+        console.log(t)
+        snake.push(t)
+
+        placeElement(t.$el, t.x, t.y)
+
+        for (i = 0; i < length; i++) {
+            var part = {}
+            part.$el = document.createElement('div')
+            part.$el.className = 'snake-body'
+            $canvas.appendChild(part.$el)
+
+            part.y = 15
+            part.x = 15 - (i+1)
+
+            placeElement(part.$el, part.x, part.y)
+
+            snake.push(part)
+        }
+
+        console.log(snake)
+    }
+
+    function cleanupSnake() {
+        var bodyPart
+
+        while (snake.length >= 1) {
+            bodyPart = snake.pop()
+            bodyPart.$el.remove()
+        }
+    }
+
     function handleKeyPress(event) {
         if (event.keyCode >= 37 && event.keyCode <= 40) {
-            direction = event.key
+            if (!direction && event.keyCode != 37) {
+
+                direction = event.key
+            }
+
+            if (['ArrowDown', 'ArrowUp'].includes(direction)
+                    && event.keyCode % 2 == 1){
+                direction = event.key
+
+            }
+            if (['ArrowLeft', 'ArrowRight'].includes(direction) &&
+                    event.keyCode % 2 == 0) {
+                direction = event.key
+            }
         }
 
         if (event.key == 'Enter') {
@@ -39,78 +106,118 @@ function main() {
             $apple = apples.shift()
             $apple.remove()
         }
-        //console.log(event.key)
+
     }
 
     function move() {
-        if (flag == true) {
+        var i
+        var tx, ty
+
+        if (!direction || (hasLost == true)) {
             return
         }
 
-        switch (direction){
-            case 'ArrowUp':
-                y -= 20
-                break
-            case 'ArrowDown':
-                y += 20
-                break
-            case 'ArrowLeft':
-                x -= 20
-                break
-            case 'ArrowRight':
-                x += 20
-                break
+
+        tx = snake[snake.length-1].x
+        ty = snake[snake.length-1].y
+
+        for (i = snake.length - 1; i >= 1; i--) {
+            snake[i].x = snake[i-1].x
+            snake[i].y = snake[i-1].y
+            placeElement(snake[i].$el, snake[i].x, snake[i].y)
         }
 
-        checkCollisions()
+        if (doGrow) {
+            var part = {}
+            part.$el = document.createElement('div')
+            part.$el.className = 'snake-body'
+            $canvas.appendChild(part.$el)
+
+            part.x = tx
+            part.y = ty
+
+            snake.push(part)
+            placeElement(part.$el, tx, ty)
+
+            doGrow = false
+        }
+
+        switch (direction) {
+            case 'ArrowUp':
+                snake[0].y--
+                break
+            case 'ArrowDown':
+                snake[0].y++
+                break
+            case 'ArrowLeft':
+                snake[0].x--
+                break
+            case 'ArrowRight':
+                snake[0].x++
+                break
+        }
+        placeElement(snake[0].$el, snake[0].x, snake[0].y)
+
+        checkBorderCollisions()
+        checkBodyCollisions()
         checkApples()
 
-        $headSnake.style.top = y + 'px'
-        $headSnake.style.left = x + 'px'
+        if (hasLost){
+            $lostMessage.style.display = 'block'
+        }
     }
 
     function resumeGame() {
-        x = 200
-        y = 200
-        $headSnake.style.left = x + 'px'
-        $headSnake.style.top = y + 'px'
-
-        flag = false
-        direction = ''
+        cleanupSnake()
+        initSnake(3)
+        hasLost = false
+        direction = null
+        $lostMessage.style.display = 'none'
     }
 
-    function checkCollisions() {
-        if (y+20 >= cy) {
-            flag = true
+    function checkBorderCollisions() {
+        if (snake[0].y >= cy) {
+            hasLost = true
         }
-        if (y <= 0) {
-            flag = true
+        if (snake[0].y <= 1) {
+            hasLost = true
         }
-        if (x <= 0) {
-            flag = true
+        if (snake[0].x <= 1) {
+            hasLost = true
         }
-        if (20+x >= cx) {
-            flag = true
+        if (snake[0].x >= cx) {
+            hasLost = true
+        }
+    }
+
+    function checkBodyCollisions() {
+        var i
+
+        for (i = 1; i < snake.length - 1; i++) {
+            if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+                hasLost = true
+            }
         }
     }
 
     function checkApples() {
         var i, apple
-        var flag = false
 
         for(i=0; i < apples.length;i++){
             apple = apples[i]
-            if (x == apple.x && y == apple.y) {
+            if (snake[0].x == apple.x && snake[0].y == apple.y) {
                 apple.$el.remove()
                 apples.splice(i, 1)
-                flag = true
+                doGrow = true
             }
         }
 
-        if (flag) {
+        if (doGrow) {
             placeApple()
-        }
+            counter++
 
+            $counter.innerHTML = 'Apples eaten: ' + counter
+        }
     }
 
     function placeApple(){
@@ -122,10 +229,10 @@ function main() {
 
         $canvas.appendChild($apple)
 
-        x = lodash.random(1, 18) * 20
-        y = lodash.random(1, 18) * 20
-        $apple.style.top = y + 'px'
-        $apple.style.left = x + 'px'
+        x = lodash.random(2, 29)
+        y = lodash.random(2, 29)
+
+        placeElement($apple, x, y)
         apples.push({
             x: x,
             y: y,
@@ -133,5 +240,8 @@ function main() {
         })
     }
 
-
+    function placeElement($el, x, y) {
+        $el.style.top = (y - 1)  * 20 + 'px'
+        $el.style.left = (x - 1)  * 20 + 'px'
+    }
 }
